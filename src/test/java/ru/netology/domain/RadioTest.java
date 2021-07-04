05ffb0d381bda95f1a0894c2e8c308cc2bcfae51
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-07-04 19:22:32 +0300

    The Force Awakens-1

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RadioTest {
-r
+
     @Test
 
     public void ShouldSetRadioWavePositive10() {
